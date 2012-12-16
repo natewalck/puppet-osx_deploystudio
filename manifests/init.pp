6afc6d44fdf8e3b105b07b3153eff2204f55771a
natewalck <nate@osxadm.in>
2012-12-16 18:51:25 -0500

    Added user.pp, updated init.pp for new vars

--- manifests/init.pp
+++ manifests/init.pp
@@ -5,13 +5,16 @@
 class deploystudio(
   $ds_role='replica',
   $ds_master_address='deploystudiomaster.domain.com',
-  $service_acct_password_hash='HashGoesHere',
+  $service_account_name='DeployStudio',
+  $service_account_hash='ShadowHashGoesHere',
+  $ds_service_account_hash='DeployStudioHashGoesHere',
 ) {
   if $::operatingsystem != 'Darwin' {
     fail('DeployStudio only runs on OS X')
   }
 
   class{'osx_deploystudio::install': } ->
+  class{'osx_deploystudio::user': } ->
   class{'osx_deploystudio::config': } ~>
   class{'osx_deploystudio::service': } ->
   Class['osx_deploystudio']
---
+++ manifests/user.pp
@@ -0,0 +1,1 @@
+# Creates the service account user
