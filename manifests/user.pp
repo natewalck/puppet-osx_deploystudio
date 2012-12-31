0ac329ee13ffe39ada02d9005e8b7fcf2867228f
natewalck <nate@osxadm.in>
2012-12-30 20:30:48 -0500

    Did some work on the users

--- manifests/user.pp
+++ manifests/user.pp
@@ -1,1 +1,12 @@
 # Creates the service account user
+
+group { 'deploystudio':
+  ensure => 'present',
+}
+
+
+user {'deploystudio':
+  ensure   => 'present',
+  groups   => $service_account_name,
+  password => $osx_service_account_hash,
+}
