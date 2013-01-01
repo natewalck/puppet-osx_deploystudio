47df06f8c3e031c04c2db0546bfa27a472f7d224
natewalck <nate@osxadm.in>
2013-01-01 12:29:00 -0500

    Added config files using templates

--- manifests/config.pp
+++ manifests/config.pp
@@ -1,4 +1,12 @@
 # Configuration class for deploystudio module
 class osx_deploystudio::config {
 
+  if $ds_role == 'master' {
+    file { '/Library/Preferences/com.deploystudio.server.plist' :
+    content => template('templates/com.deploystudio.server.master.plist.erb')
+  }
+  else {
+    file { '/Library/Preferences/com.deploystudio.server.plist' :
+    content => template('templates/com.deploystudio.server.replica.plist.erb')
+    }
 }
